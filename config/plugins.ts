25eb9d1cb2703300e8591c0629e983d6d82dc82c
mihalay123 <mihalay26@gmail.com>
2025-02-13 12:35:25 +0400

    add @strapi/provider-email-nodemailer and and plugin setting

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.gmail.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USER", "your-email@gmail.com"),
+          pass: env("SMTP_PASS", "your-email-password"),
+        },
+        secure: false, // false = использовать TLS
+      },
+      settings: {
+        defaultFrom: "your-email@gmail.com",
+        defaultReplyTo: "your-email@gmail.com",
+      },
+    },
+  },
+});
